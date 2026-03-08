880456f0221c5e1d987986574dd34ba72aa1fa78
SISU <atgsisu@gmail.com>
2026-03-08 18:16:12 +0900

    problem-solving: solve 난이도하_문자열_문자열반복_브론즈2.py

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,15 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+t = int(input())
+answer = ""
+
+for _ in range(t):
+    i1, i2 = map(str, input().split())
+    r = int(i1)
+    s = i2
+    for i in range(len(s)):
+        for _ in range(r):
+            answer += s[i]
+    answer += "\n"
+
+print(answer)
