18703185b4430fb44b56f181a72120932abcceba
SISU <atgsisu@gmail.com>
2026-03-13 17:36:43 +0900

    basic-solve: 03_quick_sort.py

--- week3/basic/03_quick_sort.py
+++ week3/basic/03_quick_sort.py
@@ -34,22 +34,19 @@ def partition(arr, low, high):
     Returns:
         피벗의 최종 위치 인덱스
     """
-    # TODO: 피벗을 선택 (일반적으로 마지막 원소)
-    pass
-    
-    # TODO: i는 작은 원소들의 마지막 인덱스를 추적
-    pass
-    
-    # TODO: low부터 high-1까지 순회하면서
-    ## 현재 원소가 피벗보다 작거나 같으면:
-    ##   1. i를 1 증가
-    ##   2. arr[i]와 arr[j]를 교환
-    pass
-    
-    # TODO: 피벗을 올바른 위치(i+1)에 배치
-    pass
-    
+    # 피벗을 선택(x)
+    x = arr[high]
+    # for j <- 0 ~ n - 1까지 돌면서 pivot(x)보다 작거나 같다면 swap
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] < x:
+            i += 1
+            arr[j], arr[i] = arr[i], arr[j]
+    # 마지막 원소(pivot)이랑 i+1을 swap
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+
     return i + 1
+
 
 def quick_sort_helper(arr, low, high):
     """
@@ -61,11 +58,15 @@ def quick_sort_helper(arr, low, high):
         high: 끝 인덱스
     """
     # TODO: base case - low가 high보다 작을 때만 정렬
+    if low >= high:
+        return
+    mid = partition(arr, low, high)
+    quick_sort_helper(arr, low, mid- 1)
+    quick_sort_helper(arr, mid + 1, high)
     ## 분할하여 피벗 인덱스 얻기
     ## 피벗 왼쪽 부분 재귀 정렬
     ## 피벗 오른쪽 부분 재귀 정렬
-    pass 
-    
+
 
 def quick_sort(arr):
     """
