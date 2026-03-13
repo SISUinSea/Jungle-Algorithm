2790409e504e5cc5318a2706ee304fa98e53a1a1
SISU <atgsisu@gmail.com>
2026-03-13 17:57:44 +0900

    solve: 난이도하_큐_카드2_실버4.py

--- week3/problem-solving/난이도하_큐_카드2_실버4.py
+++ week3/problem-solving/난이도하_큐_카드2_실버4.py
@@ -1,2 +1,17 @@
 # 큐 - 카드2 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/2164
+
+from collections import deque
+
+deck = deque(range(1, int(input()) + 1))
+
+i = 1
+while len(deck) > 1:
+    if i % 2 == 1: # 홀수인 경우 카드 버리기
+        deck.popleft()
+    else:
+        deck.append(deck.popleft())
+    i+=1
+
+print(deck.popleft())
+
