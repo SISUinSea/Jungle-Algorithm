067bac581a27be2bb84a677ef978914a5e9d3a5c
SISU <atgsisu@gmail.com>
2026-03-13 18:10:44 +0900

    solve: 난이도하_스택_스택_실버4.py

--- week3/problem-solving/난이도하_스택_스택_실버4.py
+++ week3/problem-solving/난이도하_스택_스택_실버4.py
@@ -1,2 +1,17 @@
 # 스택 - 스택 (백준 실버 4)
 # 문제 링크: https://www.acmicpc.net/problem/10828
+
+instruction_count = int(input())
+stack = []
+for _ in range(instruction_count):
+    instruction = input().split()
+    if instruction[0] == 'push':
+        stack.append(instruction[1])
+    elif instruction[0] == 'pop':
+        print(-1 if not stack else stack.pop())
+    elif instruction[0] == 'size':
+        print(len(stack))
+    elif instruction[0] == 'empty':
+        print(0 if stack else 1)
+    elif instruction[0] == 'top':
+        print(stack[-1] if stack else -1)
