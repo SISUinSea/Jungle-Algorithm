5af532eb4e52198e8408fc310e5a52d5a1f6af04
SISU <atgsisu@gmail.com>
2026-03-16 14:21:44 +0900

    solve: 난이도중_해시테이블_세수의합_골드4.py

--- week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
+++ week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
@@ -1,2 +1,27 @@
 # 해시 테이블 - 세 수의 합 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/2295
+n = int (input())
+
+s = set()
+a = []
+for _ in range(n):
+    d = int(input())
+    a.append(d)
+
+a.sort(reverse=True)
+
+
+for element in a:
+    for element2 in a:
+        s.add(element + element2)
+
+
+# 가장 큰 수부터 탐색하기, 답을 찾으면 종료
+## 가장 큰 수 - 어떤 수 in {2쌍의 합} 이면 반환 후 종료
+
+
+for target in a:
+    for j in a:
+        if target - j in s:
+            print(target)
+            exit()
