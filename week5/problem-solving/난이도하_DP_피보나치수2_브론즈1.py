3e2e460d7745ce36f98a12a7e43e21cc9717513b
SISU <atgsisu@gmail.com>
2026-03-31 22:37:08 +0900

    solve: 난이도하

--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,12 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+
+n = int(input())
+
+dp = [0] * max(n + 1, 2 + 1)
+dp[1] = dp[2] = 1
+if n >= 3:
+    for i in range(3, n + 1):
+        dp[i] = dp[i-1] + dp[i - 2]
+
+print(dp[n])
